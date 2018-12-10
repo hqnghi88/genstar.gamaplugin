ab7949fdf1cc165373af2dcf39999c8c00b151a8
benoitgaudou <benoit.gaudou@gmail.com>
2018-12-10 15:27:20 +0700

    Correction of build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-cd genstar.parent &&
+cd genstar.gamaplugin.parent &&
 mvn clean install -U &&
 cd - &&
-cd genstar.p2updatesite &&
+cd genstar.gamaplugin.p2updatesite &&
 mvn -U clean install -P p2Repo --settings ../settings.xml && 
 cd

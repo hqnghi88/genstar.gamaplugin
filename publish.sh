8d129c22d94608d1bc9ca2c680ad7291b3a8c8e1
benoitgaudou <benoit.gaudou@gmail.com>
2018-12-11 00:45:44 +0700

    update

---
+++ publish.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+bash ./pushToBintray.sh hqnghi88 $BINTRAY ctu gama_genstar "genstar.plugin.bundle.all.feature" "1.0.0" /home/travis/build/hqnghi88/gamaplugin/genstar.p2updatesite/target/p2Repo
